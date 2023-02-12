c8fdcc69ffb3326a4dd50244f20d969faa15a198
Caio Neves <ncaio037@gmail.com>
2023-02-11 23:29:18 -0300

    Atualizando o bot em seus slashCommands e permissões, além de adicionar comando básicos para testar suas funcionalidade.

--- index.js
+++ index.js
@@ -1,55 +1,71 @@
-const Discord = require("discord.js");
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-//Importando as Intents(permissão do Bot).
+const dotenv = require('dotenv');
+dotenv.config();
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
-const client = new Discord.Client({ 
-    intents: [ 
-        "Guilds", 
-        "GuildMembers", 
-        "MessageContent", 
-        "GuildMessages" 
-    ]
+
+
+//Importação de comandos
+const fs = require('node:fs');
+const path = require('node:path');
+
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+client.commands = new Collection();
+
+for(file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	
+	if("data" in command && "execute" in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`Esse comando em ${filePath} está com data ou execute ausentes.`)
+	}
+}
+
+// Log in Bot
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-require('dotenv').config();
+client.login(TOKEN);
 
-module.exports = client;
+//Listener de interações com o bot
+client.on(Events.InteractionCreate, async interation =>{
+	if (interaction.isStringSelectMenu()){
+        const selected = interaction.values[0]
+        if (selected == "javascript"){
+            await interaction.reply("Documentação do Javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript")
+        } 
+		else if (selected == "python"){
+            await interaction.reply("Documentação do Python: https://www.python.org")
+        } 
+		else if (selected == "csharp"){
+            await interaction.reply("Documentação do C#: https://learn.microsoft.com/en-us/dotnet/csharp/")
+        } 
+		else if (selected == "discordjs"){
+            await interaction.reply("Documentação do Discord.js: https://discordjs.guide/#before-you-begin")
+        }
+    }
 
-//Quando acontecer alguma interação do tipo ApplicationCommand com o bot. 
-client.on('interactionCreate', (interaction) => {
-
-    if(interaction.type === Discord.InteractionType.ApplicationCommand){
-        
-        //pega o nome do comando que o usuário está solicitando
-        const cmd = client.slashCommands.get(interaction.commandName);
-        
-        //Retorna um erro se não existir esse comando.
-        if (!cmd) return interaction.reply(`Error`);
-  
-        interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-        
-        //Executando o comando solicitado.
-        cmd.run(client, interaction);
-  
-     }
-});
-
-//Quando o Bot estiver Online, ele executará essas instruções.
-
-client.on('ready', () => {
-    console.log(`${client.user.username} está Online.`);
-
-    client.user.setActivity({ 
-        name: 'Estudando sobre JavaScript e a Library Discord.js V14.',
-        type: Discord.ActivityType.Streaming
-    });
-});
-
-//Coleção que armazena os comandos do Bot.
-client,slashCommands = new Discord.Collection();
-
-require('./handler')(client);
-
-client.login(process.env.TOKEN);
-
+	if(!interation.isChatInputCommand()) return;
+ 	
+	const command = interation.client.commands.get(interation.commandName);
+	
+	if(!command){
+		console.error(`Esse comando não existe`);
+		return
+	} 
+	
+	try{
+		await command.execute(interation);
+	} catch(error) {
+		console.error();
+		await interation.reply("Houve um erro ao executar esse comando")
+	};
+});  
 
